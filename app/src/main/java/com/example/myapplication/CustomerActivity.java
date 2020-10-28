a2dbed5b0b2920516c9bca2f83c444351056b692
yuyanchua <ychua7@wisc.edu>
2020-10-28 00:40:42 -0500

    Implement some code to customer and feedback

--- app/src/main/java/com/example/myapplication/CustomerActivity.java
+++ app/src/main/java/com/example/myapplication/CustomerActivity.java
@@ -1,4 +1,42 @@
 package com.example.myapplication;
 
-public class CustomerActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView();
+        Button btFeedback = null;
+        btFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toFeedback();
+            }
+        });
+
+
+    }
+
+    private void toFeedback(){
+        startActivity(new Intent(CustomerActivity.this, FeedbackActivity.class));
+    }
+
+    private void toTimeline(){
+//        startActivity(new Intent(CustomerActivity.this, TimelineActivity.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        Intent intent = new Intent(CustomerActivity.this, ProjectMainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/myapplication/FeedbackActivity.java
+++ app/src/main/java/com/example/myapplication/FeedbackActivity.java
@@ -1,4 +1,62 @@
 package com.example.myapplication;
 
-public class FeedbackActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebase;
+    DatabaseReference db_ref;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView();
+
+        firebase = FirebaseDatabase.getInstance();
+        db_ref = firebase.getReference("Project");
+
+        Button btConfirm = null;
+        btConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitFeedback();
+            }
+        });
+
+    }
+
+    private void submitFeedback(){
+        EditText feedEdit = null;
+        String feedback = feedEdit.getText().toString();
+        addFeedbackToDatabase(feedback);
+    }
+
+    private void addFeedbackToDatabase(final String feedback){
+        db_ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String projectId = Long.toString(Project.projectId);
+                //TODO: add feedback class, with id, username, comment
+//                dataSnapshot.child(projectId).child("feedback");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
