08230f85a6059240672c9ac9b6e2598f2fc828b8
yuyanchua <ychua7@wisc.edu>
2020-11-10 22:16:01 -0600

    Add searching class file

---
+++ app/src/test/java/com/MyApplicationTest/SearchTest.java
@@ -0,0 +1,41 @@
+package com.MyApplicationTest;
+
+import org.junit.Test;
+
+import com.example.myapplication.engine.SearchProject;
+
+public class SearchTest {
+
+    @Test
+    void testSearch_existedProject(){
+        SearchProject testSearch = new SearchProject("project");
+
+    }
+
+    @Test
+    void testSearch_existedUser(){
+
+    }
+
+    @Test
+    void testSearch_emptyProjectField(){
+
+    }
+
+    @Test
+    void testSearch_emptyUserField(){
+
+    }
+
+    @Test
+    void testSearch_notExistProject(){
+
+    }
+
+    @Test
+    void testSearch_notExistUser(){
+
+    }
+
+
+}
