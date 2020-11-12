5da157e895fd9902f09bf5a99b8fe5aabe6cac47
ylong7437 <56174199+ylong7437@users.noreply.github.com>
2020-11-11 22:48:29 -0600

    Hide action bar

--- app/src/main/java/com/example/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
 
